2474e64ee4848ee5bcd70105fe0442b4178adecf
Natasha Maruska Navarro <nmfnavarro@gmail.com>
2021-01-25 18:21:54 +0800

    car item component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CartItem from './components/CartItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+    <CartItem ></CartItem>
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +16,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+ 
 });
